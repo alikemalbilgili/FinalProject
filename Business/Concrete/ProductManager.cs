70ee53f0978b9708688ef1cc71e1da434db4988b
Ali Kemal Bilgili <bilgilialikemal56@gmail.com>
2022-05-27 17:40:55 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //iş kodları
+            //Yetkisi var mı ? ...
+            return _productDal.GetAll();
+
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Abstract
+{
+    //IEntity implement eden bir class bir veritabanı tablosudur.
+    //A class which implements to IEntity  is a database table
+    public interface IEntity
+    {
+
+    }
+}
