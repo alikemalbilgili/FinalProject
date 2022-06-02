4d2b03351eb8fcf5df6ab91481ef299ba19f6689
erenozgun <erennozgunn06@gmail.com>
2022-06-02 15:41:40 +0300

    Core layer added

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,18 +1,17 @@
-﻿using Entities.Abstract;
+﻿
+using Core.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+
+namespace Core.DataAccess
 {
     //Generic constraint
     // Class: ref tip olabilir 
-    public interface IEntityRepository<T> where T : class,IEntity,new()
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
-        List<T> GetAll(Expression<Func<T, bool>> filter=null);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
