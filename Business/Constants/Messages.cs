a5c78444430929bddf3d9879cf0d0546130f568d
alikemalbilgili <bilgilialikemal56@gmail.com>
2022-06-13 17:12:28 +0300

    Fluent Validation and aspect were added

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -12,5 +12,6 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Product name is invalid";
         public static string MaintenanceTime = "Sorry,maintenance time";
         public static string ProductListed = "Products were listed";
+        internal static string UnitPriceInvalid;
     }
 }
